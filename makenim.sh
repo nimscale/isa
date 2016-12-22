da46549db5fdb7b22ecdc13b75aa065178ec7c39
andrzejslomskii <andrzejslomski@engineer.com>
2016-12-22 06:34:40 -0800

    Add files via upload

--- makenim.sh
+++ makenim.sh
@@ -1,2 +1,5 @@
+#for ISA-L-master
 c2nim  --prefix:isa_ --dynlib:libname --cdecl gf_vect_mul.h --out:gf_vect_mul.nim
 c2nim  --prefix:isa_ --dynlib:libname --cdecl erasure_code.h --out:erasure_code.nim
+
+#for ISA-L_Cryptor
