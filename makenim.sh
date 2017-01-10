ae779d9e2875682be9e30beee623cf25d80f1048
andrzejslomskii <andrzejslomski@engineer.com>
2017-01-10 06:29:26 +0100

    Remove the "_" from the file name

--- makenim.sh
+++ makenim.sh
@@ -1,4 +1,4 @@
 #for ISA-L-master
-c2nim  --prefix:isa_ --dynlib:libname --cdecl include/gf_vect_mul.h --out:src/gf_vect_mul.nim
-c2nim  --prefix:isa_ --dynlib:libname --cdecl include/erasure_code.h --out:src/erasure_code.nim
+c2nim  --prefix:isa_ --dynlib:libname --cdecl include/gf_vect_mul.h --out:src/gfvectmul.nim
+c2nim  --prefix:isa_ --dynlib:libname --cdecl include/erasure_code.h --out:src/erasurecode.nim
 #for ISA-L_Cryptor
